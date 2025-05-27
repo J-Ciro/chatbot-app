5758b731e81ce9b429872e95084efb29794983b6
J-Ciro <jciro.1984@gmail.com>
2025-05-26 22:58:47 -0500

    fix(railway): adapted databse file for railway

--- backend/src/config/database.ts
+++ backend/src/config/database.ts
@@ -3,13 +3,26 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
+// const database = mysql.createPool({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME,
+//   waitForConnections: true,
+//   port: Number(process.env.MYSQLPORT) || 3306,
+//   connectionLimit: 10,
+//   ssl: {
+//     rejectUnauthorized: false,
+//   },
+// });
+
 const database = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  waitForConnections: true,
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
   port: Number(process.env.MYSQLPORT) || 3306,
+  waitForConnections: true,
   connectionLimit: 10,
   ssl: {
     rejectUnauthorized: false,
