34cb468605aedd06edbf63e1c8adcfb144532779
J-Ciro <jciro.1984@gmail.com>
2025-05-26 23:16:08 -0500

    fix(railway): readded db config for railway

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -6,5 +6,5 @@ dotenv.config();
 const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
-  console.log(`🚀 Servidor backend corriendo en http://localhost:${PORT}`);
+  console.log(`🚀 Servidor backend corriendo`);
 });
